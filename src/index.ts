3cbacbe9fa3f1bedcd92a93810aeb8ae3ba37fc1
DavidHeymann <heymann.david@gmai.com>
2021-07-15 18:47:17 +0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,6 @@ app.use(json());
 app.use('/api/user/', userRouter);
 app.use('/api/video/', VideoRouter);
 
-app.listen(3000, () => {
+app.listen(3002, () => {
   console.log(`server is listening on port 3000`);
 });
